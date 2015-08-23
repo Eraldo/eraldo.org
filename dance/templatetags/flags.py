f47f41803d8cac3ffbabdfe29c531984ef9a336d
Eraldo Helal <eraldo@eraldo.org>
2015-08-23 22:10:44 +0200

    Fixed a strange country name.

--- dance/templatetags/flags.py
+++ dance/templatetags/flags.py
@@ -10,4 +10,7 @@ __author__ = 'eraldo'
 @register.simple_tag
 def flag(country):
     country_code = countries.by_name(country)
+    if not country_code:
+        if country == 'United Kingdom':
+            country_code = 'GB'
     return render_to_string('dance/_flag.html', {'country': country, 'code': country_code})
